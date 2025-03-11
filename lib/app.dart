598bc5ce2f6c8862cd022025f45dcbbbbf0e8607
pavelantoshkin <paavoantoshkin@gmail.com>
2025-03-11 11:00:29 +0100

    task: update sorting favourites by breed

---
+++ lib/app.dart
@@ -0,0 +1,38 @@
+import 'package:flashcards_flutter/core/di/locator.dart';
+import 'package:flashcards_flutter/core/presentation/router.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'core/localization/generated/l10n.dart';
+
+class DoggyGramApp extends StatelessWidget {
+  const DoggyGramApp({super.key});
+  static final _l10n = getIt<I10n>();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      localizationsDelegates: [
+        I10n.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: I10n.delegate.supportedLocales,
+      localeResolutionCallback: (deviceLocale, supportedLocales) {
+        if (supportedLocales
+            .map((e) => e.languageCode)
+            .contains(deviceLocale?.languageCode)) {
+          return deviceLocale;
+        } else {
+          return const Locale('en', '');
+        }
+      },
+      title: _l10n.app_name,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
+      ),
+      routerConfig: mainRouter,
+    );
+  }
+}
